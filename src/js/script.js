e3863dcb2bb4fa3720727bd18b74e58ae9d67a30
Joan Viray <virayjoan1985@gmail.com>
2024-04-10 01:25:31 +0800

    update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -258,17 +258,7 @@ function displayQueue() {
     });
 }
 
-// Function to create a movie element
-function createMovieElement(movie) {
-    const movieElement = document.createElement('div');
-    movieElement.classList.add('movie');
-    movieElement.innerHTML = `
-        <h2>${movie.title}</h2>
-        <p>${movie.overview}</p>
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
-    `;
-    return movieElement;
-}
+
 
 
 
